e4ed76aed55453feee5ada2d08592ee188d1ed8f
maszhar <fikrimustofa024@gmail.com>
2025-03-07 22:32:02 +0700

    hilangkan spasi pada nama bawaan class

--- src/renderer/src/common/entitas/Class.ts
+++ src/renderer/src/common/entitas/Class.ts
@@ -1,7 +1,7 @@
 import { Komponen } from './Komponen'
 
 export class Class extends Komponen {
-  constructor(nama: string = 'Class Baru') {
+  constructor(nama: string = 'ClassBaru') {
     super(nama)
   }
 }
