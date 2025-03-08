3574f5bbb2bc41c98ae15b0803c918f960bc9782
maszhar <fikrimustofa024@gmail.com>
2025-03-08 21:41:37 +0700

    sesuaikan tampilan kursor mulai koneksi dengan posisi kursor

--- src/renderer/src/sequence/ui/TampilanKursorMulaiKoneksi.svelte
+++ src/renderer/src/sequence/ui/TampilanKursorMulaiKoneksi.svelte
@@ -1,3 +1,15 @@
-<div class="absolute rounded-full w-4 h-4 bg-sky-200 flex justify-center items-center">
+<script lang="ts">
+  import { Koordinat } from '../../common/entitas/Koordinat'
+
+  interface Properti {
+    posisi?: Koordinat
+  }
+  const { posisi = new Koordinat() }: Properti = $props()
+</script>
+
+<div
+  class="absolute rounded-full w-4 h-4 bg-sky-200 flex justify-center items-center pointer-events-none"
+  style={`left: ${posisi.x}px; top: ${posisi.y}px;`}
+>
   <div class="rounded-full w-2 h-2 bg-sky-400"></div>
 </div>
--- src/renderer/src/sequence/ui/TampilanSpesifikasiEksekusi.svelte
+++ src/renderer/src/sequence/ui/TampilanSpesifikasiEksekusi.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+  import { Koordinat } from '../../common/entitas/Koordinat'
   import TampilanKursorMulaiKoneksi from './TampilanKursorMulaiKoneksi.svelte'
 
   interface Properti {
@@ -6,28 +8,45 @@
   }
   const { indeks }: Properti = $props()
 
+  let elemen: Node
+  let titikAtas = 0
+
+  let posisiKursorMulaiKoneksi: Koordinat | null = $state(null)
   let mouseMelayang = $state(false)
   function tanganiMouseMelayang(): void {
     mouseMelayang = true
   }
 
   function tanganiMouseMeninggalkan(): void {
+    posisiKursorMulaiKoneksi = null
     mouseMelayang = false
   }
+
+  function tanganiMouseGerak(e: MouseEvent): void {
+    if (mouseMelayang) {
+      posisiKursorMulaiKoneksi = new Koordinat(0, e.clientY - titikAtas + 14)
+    }
+  }
+
+  onMount(() => {
+    titikAtas = (elemen as HTMLElement).getBoundingClientRect().y
+  })
 </script>
 
 <div
   class="absolute top-5"
   onmouseenter={(): void => tanganiMouseMelayang()}
+  onmousemove={(e: unknown): void => tanganiMouseGerak(e as MouseEvent)}
   onmouseleave={(): void => tanganiMouseMeninggalkan()}
   role="button"
   tabindex={2000 + indeks}
+  bind:this={elemen}
 >
   <svg width="10" height="200" xmlns="http://www.w3.org/2000/svg">
     <rect x="1" width="8" height="100" fill="#ccc" stroke="black" />
   </svg>
 </div>
 
-{#if mouseMelayang}
-  <TampilanKursorMulaiKoneksi />
+{#if mouseMelayang && posisiKursorMulaiKoneksi !== null}
+  <TampilanKursorMulaiKoneksi posisi={posisiKursorMulaiKoneksi} />
 {/if}
