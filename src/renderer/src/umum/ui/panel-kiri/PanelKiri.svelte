545bcc049c03e583b340657e5e09681b09542f9b
maszhar <fikrimustofa024@gmail.com>
2025-03-18 00:24:12 +0700

    tampilan pohon komponen proyek

--- src/renderer/src/umum/ui/panel-kiri/PanelKiri.svelte
+++ src/renderer/src/umum/ui/panel-kiri/PanelKiri.svelte
@@ -1,1 +1,13 @@
-<div class="w-50 bg-white border-r-2 border-zinc-400 flex flex-none"></div>
+<script lang="ts">
+  import TampilanItemKomponenProyek from './komponen/TampilanItemKomponenProyek.svelte'
+</script>
+
+<div class="w-50 bg-white border-r-2 border-zinc-400 flex flex-col flex-none p-3 overflow-auto">
+  <TampilanItemKomponenProyek>Proyek</TampilanItemKomponenProyek>
+  <TampilanItemKomponenProyek level={1}>Class Diagram</TampilanItemKomponenProyek>
+  <TampilanItemKomponenProyek level={2}>Class Diagram 1</TampilanItemKomponenProyek>
+  <TampilanItemKomponenProyek level={2}>Class Diagram 2</TampilanItemKomponenProyek>
+  <TampilanItemKomponenProyek level={1}>Sequence Diagram</TampilanItemKomponenProyek>
+  <TampilanItemKomponenProyek level={2}>Sequence Diagram 1</TampilanItemKomponenProyek>
+  <TampilanItemKomponenProyek level={2}>Sequence Diagram 2</TampilanItemKomponenProyek>
+</div>
---
+++ src/renderer/src/umum/ui/panel-kiri/komponen/TampilanItemKomponenProyek.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+
+  interface Properti {
+    children?: Snippet
+    level?: number
+  }
+  const { children, level = 0 }: Properti = $props()
+</script>
+
+<button class="flex justify-start">
+  {#if level > 0}
+    <svg class="h-4 flex-none" style={`width: ${18 * level}px`}></svg>
+  {/if}
+  <div class="w-4 h-4 bg-red-500 mt-0.5"></div>
+  <div class="text-sm text-left ms-1">
+    {@render children?.()}
+  </div>
+</button>
