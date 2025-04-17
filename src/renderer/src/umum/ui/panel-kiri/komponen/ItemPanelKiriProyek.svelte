b661cd21bd5d2df40f612b3e388f1dcc80c18d36
maszhar <fikrimustofa024@gmail.com>
2025-04-18 03:43:15 +0700

    ikon proyek

--- src/renderer/src/umum/ui/panel-kiri/komponen/ItemPanelKiriProyek.svelte
+++ src/renderer/src/umum/ui/panel-kiri/komponen/ItemPanelKiriProyek.svelte
@@ -2,6 +2,7 @@
   import type { IsiProyek } from '../../../../../../umum/entitas/IsiProyek'
   import type { Koordinat } from '../../../../../../umum/entitas/Koordinat'
   import type { ProyekLangsung } from '../../../entitas/ProyekLangsung.svelte'
+  import IkonProyek from '../../ikon/IkonProyek.svelte'
   import { JenisMenuPanelKiri } from '../JenisMenuPanelKiri'
   import ItemPanelKiri from './ItemPanelKiri.svelte'
   import ItemPanelKiriSistem from './ItemPanelKiriSistem.svelte'
@@ -26,6 +27,9 @@
   bukaMenu={(posisi: Koordinat): void => bukaMenu(posisi, JenisMenuPanelKiri.PROYEK)}
   id={idProyek}
 >
+  {#snippet ikon()}
+    <IkonProyek class="w-full h-full" />
+  {/snippet}
   {#snippet itemChildren(level: number)}
     {#each koleksiSistem as sistem, _i (sistem.dapatkanId())}
       <ItemPanelKiriSistem {level} {sistem} {idAktif} {pilih} {bukaMenu} idPrefix={idProyek} />
